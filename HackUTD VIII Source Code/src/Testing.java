f82ff80811146506aed0e3619be9313c593cbd97
Ckimbel1 <66196217+Ckimbel1@users.noreply.github.com>
2021-11-13 10:48:48 -0600

    Update Testing.java

--- HackUTD VIII Source Code/src/Testing.java
+++ HackUTD VIII Source Code/src/Testing.java
@@ -1,9 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 public class Testing {
 	public static void main(String[] args) {
 		BufferedReader file = new BufferedReader(new InputStreamReader(System.in));
-		
+		PrintWriter out = new PrintWriter(System.out);
 	}
 }
