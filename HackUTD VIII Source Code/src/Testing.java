36ba922cb1e8c4cc3c6330a3019f70cd40fd8edc
GabrielaHuerta <39127090+GabrielaHuerta@users.noreply.github.com>
2021-11-13 10:43:51 -0600

    Update Testing.java

--- HackUTD VIII Source Code/src/Testing.java
+++ HackUTD VIII Source Code/src/Testing.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Testing {
-
+	public static void main(String[] args) {
+		BufferedReader file = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
 }
