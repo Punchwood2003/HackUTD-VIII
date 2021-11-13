295183323f8e87df675f6901a73e6729a995e75c
Punchwood2003 <punchwood2003@gmail.com>
2021-11-13 16:04:34 -0600

    Fixed the .bat files

--- HackUTD VIII Source Code/ScrapeApeWisdom_Cryptos.bat
+++ HackUTD VIII Source Code/ScrapeApeWisdom_Cryptos.bat
@@ -1,5 +1,3 @@
 @echo off
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://apewisdom.io/api/v1.0/filter/all-stocks/', 'top100Stocks.txt')"
-for /f "tokens=*" %%A in (top100Stocks) do (echo%%A)
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://apewisdom.io/api/v1.0/filter/all-crypto', 'top100Cryptos.txt')"
 for /f "tokens=*" %%A in (top100Cryptos.txt) do (echo%%A)
---
+++ HackUTD VIII Source Code/ScrapeApeWisdom_Stocks.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://apewisdom.io/api/v1.0/filter/all-stocks/', 'top100Stocks.txt')"
+for /f "tokens=*" %%A in (top100Stocks) do (echo%%A)
