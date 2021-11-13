fe4e2987ea302e451fe2589b452bd64319cf1e40
GabrielaHuerta <39127090+GabrielaHuerta@users.noreply.github.com>
2021-11-13 14:25:01 -0600

    Update TestProcess.java

--- HackUTD VIII Source Code/src/TestProcess.java
+++ HackUTD VIII Source Code/src/TestProcess.java
@@ -7,7 +7,7 @@ public class TestProcess
 	public static void main(String[]args) throws IOException
 	{
 		ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "text.bat");
-		File dir = new File("(add file location)");
+		File dir = new File("C:/Documents/GitHub");
 		pb.directory(dir);
 		Process p = pb.start();
 		
