57ebc940b1803ba31b4149a6a1dd522ff02a2941
Ckimbel1 <66196217+Ckimbel1@users.noreply.github.com>
2021-11-14 05:30:15 -0600

    Updating Testing File

--- social-media-stock-exchange/src/test/java/com/hackutd/viii/SocialMediaStockExchangeApplicationTests.java
+++ social-media-stock-exchange/src/test/java/com/hackutd/viii/SocialMediaStockExchangeApplicationTests.java
@@ -1,12 +1,17 @@
 package com.hackutd.viii;
+import javax.swing.Timer;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.hackutd.viii.social.TopTickers;
 import com.hackutd.viii.yahoofinance.StockService;
 import com.hackutd.viii.yahoofinance.StockWrapper;
 
@@ -28,5 +33,27 @@ class SocialMediaStockExchangeApplicationTests {
 		
 		final BigDecimal mean20DayPercent = stockService.findChangeFrom20MeanPercent(stock);
 		System.out.println(mean20DayPercent);
+	}
+	
+	@Test
+	void multiple() throws IOException, InterruptedException {
+		TopTickers topTickers = new TopTickers();
+		String[] top100StockTickersArr = topTickers.getTopStockTickers();
+		ArrayList<String> asList = this.toList(top100StockTickersArr);
+		final List<StockWrapper> stocks = stockService.findStocks(asList);
+		
+		for(StockWrapper currWrapper : stocks) {
+			final BigDecimal price = stockService.findPrice(currWrapper);
+			System.out.println(currWrapper.getStock() + ": ");
+		}
+		
+	}
+	
+	public ArrayList<String> toList(String[] arr) {
+		ArrayList<String> list = new ArrayList<String>();
+		for(String temp : arr) {
+			list.add(temp);
+		}
+		return list;
 	}
 }
