34bf76e15144f200a4af50498b659d11ea8314df
Punchwood2003 <punchwood2003@gmail.com>
2021-11-13 18:29:45 -0600

    Added full implementation of Reddit Data

---
+++ HackUTD VIII Source Code/src/Crypto.java
@@ -0,0 +1,131 @@
+/**
+ * This class handles all of the important information about 
+ * any given Cryptocurrency. This includes information like the rank
+ * of that currency according to Reddit, the currency's ticker, the 
+ * full name of the currency, the current mentions and the mentions 
+ * 24 hours ago, and the current upvotes and the upvotes 24 hours ago.
+ * @author MatthewSheldon
+ */
+public class Crypto implements Comparable<Crypto> {
+	/**
+	 * The rank of the Crypto
+	 */
+	private byte rank; 
+	
+	/**
+	 * The ticker of the Crypto
+	 */
+	private String ticker;
+	
+	/**
+	 * The full name of the Crypto
+	 */
+	private String name;
+	
+	/**
+	 * The number of mentions of the Crypto
+	 */
+	private long mentions;
+	
+	/**
+	 * The number of upvotes of the Crypto
+	 */
+	private long upvotes;
+	
+	/**
+	 * The rank of the Crypto 24 hours ago
+	 */
+	private int rank_24h_ago;
+	
+	/**
+	 * The number of mentions of the Crypto 24 hours ago
+	 */
+	private long mentions_24h_ago;
+	
+	/**
+	 * Constructs a Crypto object with the passed parameters
+	 * @param rank			The rank on Reddit
+	 * @param ticker		The ticker
+	 * @param name			The full name
+	 * @param mentions		The number of mentions on Reddit
+	 * @param upvotes		The number of upvotes on Reddit
+	 * @param rank24h		The rank on Reddit 24 hours ago
+	 * @param mentions24h	The number of mentions on Reddit 24 hours ago
+	 */
+	public Crypto(byte rank, String ticker, String name, long mentions, long upvotes, int rank24h, long mentions24h) {
+		this.rank = rank;
+		this.ticker = ticker;
+		this.name = name;
+		this.mentions = mentions;
+		this.upvotes = upvotes;
+		this.rank_24h_ago = rank24h;
+		this.mentions_24h_ago = mentions24h;
+	}
+
+	/**
+	 * @return the rank
+	 */
+	public byte getRank() {
+		return rank;
+	}
+
+	/**
+	 * @return the ticker
+	 */
+	public String getTicker() {
+		return ticker;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the mentions
+	 */
+	public long getMentions() {
+		return mentions;
+	}
+
+	/**
+	 * @return the upvotes
+	 */
+	public long getUpvotes() {
+		return upvotes;
+	}
+
+	/**
+	 * @return the rank 24 hours ago
+	 */
+	public int getRank_24h_ago() {
+		return rank_24h_ago;
+	}
+
+	/**
+	 * @return the mentions 24 hours ago
+	 */
+	public long getMentions_24h_ago() {
+		return mentions_24h_ago;
+	}
+	
+	@Override
+	public int compareTo(Crypto other) {
+		return Byte.compare(this.rank, other.rank);
+	}
+	
+	@Override
+	public String toString() {
+		String output = "";
+		output += "Name:\t\t\t" + this.name;
+		output += "\nTicker:\t\t\t" + this.ticker;
+		output += "\nRank:\t\t\t" + this.rank;
+		output += "\nRank 24 Hours Ago:\t" + this.rank_24h_ago;
+		output += "\nMentions:\t\t" + this.mentions;
+		output += "\nMentions 24 Hours Ago:\t" + this.mentions_24h_ago;
+		output += "\nUpvotes:\t\t" + this.upvotes;
+		return output;
+	}
+}
---
+++ HackUTD VIII Source Code/src/CryptoReader.java
@@ -0,0 +1,126 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * This class handles requesting an updated list of the 
+ * top 100 Crypto Currencies on Reddit, reading in the data,
+ * and initializing a map between a tinker and its respective
+ * Crypto object
+ * @author MatthewSheldon
+ */
+public class CryptoReader {
+	/**
+	 * A HashMap that maps a Cryptocurrency's ticker to its info.
+	 * This is where the top 100 cryptocurrencies can be referenced
+	 * @author MatthewSheldon
+	 */
+	private HashMap<String, Crypto> tickerToCryptoInfo;
+	
+	/**
+	 * An array indicating the top 100 tickers
+	 * @author MatthewSheldon
+	 */
+	private String[] top100Tickers;
+	
+	/**
+	 * The file directory to read from
+	 * @author MatthewSheldon
+	 */
+	private String fileName = "top100Cryptos.txt";
+	
+	public static void main(String[] args) throws IOException {
+		new CryptoReader().run();
+	}
+	
+	/**
+	 * This function when run will read in from the "top100Cryptos" file
+	 * and initialize the tickerSymbols array containing the top 100 crypto
+	 * Ticker Symbols.
+	 * @throws IOException
+	 * @author MatthewSheldon
+	 */
+	public void run() throws IOException {
+		// Create a BufferedReader to read from the top100Cryptos.txt file
+		BufferedReader file = new BufferedReader(new FileReader(fileName));
+		// Read in the entire line of input
+		String line = file.readLine();
+		// Remove unimportant text
+		int index1 = line.indexOf('[') + 1;
+		int index2 = line.indexOf(']');
+		line = line.substring(index1, index2);
+		
+		// Initialize the HashMap
+		tickerToCryptoInfo = new HashMap<String, Crypto>();
+		// Initialize the top 100 tinkers
+		top100Tickers = new String[100];
+		
+		// Parse the line into individual cryptos
+		String[] rawInfo = line.split("},");
+		
+		// Special case where the last data point doesn't have a comma after the brace
+		rawInfo[99] = rawInfo[99].substring(0, rawInfo[99].length()-1);
+		
+		// Format each raw crypto
+		for(int i = 0; i < rawInfo.length; i++) {
+			// Remove the curly braces from the start of the current crypto info
+			rawInfo[i] = rawInfo[i].substring(1);
+			
+			// Parse the info by the comma and assign variables to each "raw" representation of these values
+			String[] formattedInfo = rawInfo[i].split(",\"");
+			// Formatted Rank
+			formattedInfo[0] = formattedInfo[0].replaceAll("\"", "").substring("rank:".length());
+			// Formatted Tinker
+			formattedInfo[1] = formattedInfo[1].replaceAll("\"", "").substring("ticker:".length());
+			// Formatted Name
+			formattedInfo[2] = formattedInfo[2].replaceAll("\"", "").substring("name:".length());
+			// Formatted mentions
+			formattedInfo[3] = formattedInfo[3].replaceAll("\"", "").substring("mentions:".length());
+			// Formatted upvotes
+			formattedInfo[4] = formattedInfo[4].replaceAll("\"", "").substring("upvotes:".length());
+			// Formatted rank 24 hours ago
+			formattedInfo[5] = formattedInfo[5].replaceAll("\"", "").substring("rank_24h_ago:".length());
+			// Formatted mentions 24 hours ago
+			formattedInfo[6] = formattedInfo[6].replaceAll("\"", "").substring("mentions_24h_ago:".length());
+			
+			// Special case where any of these values are zero (API represents 0 as null)
+			for(int j = 0; j <= 6; j++) {
+				if(formattedInfo[j].equals("null")) {
+					formattedInfo[j] = "0";
+				}
+			}
+			
+			// Instantiate all of the variables that will be used to create the current crypto object
+			byte currRank;
+			String currTicker, currName;
+			int currRank24h;
+			long currMentions, currUpvotes, currMentions24h;
+			
+			// Parse all of the info into their respective data types
+			currRank = Byte.parseByte(formattedInfo[0]);
+			currTicker = formattedInfo[1];
+			currName = formattedInfo[2];
+			currMentions = Long.parseLong(formattedInfo[3]);
+			currUpvotes = Long.parseLong(formattedInfo[4]);
+			currRank24h = Integer.parseInt(formattedInfo[5]);
+			currMentions24h = Long.parseLong(formattedInfo[6]);			
+			
+			// Create the current Crypto object
+			Crypto currCrypto = new Crypto(currRank, currTicker, currName, currMentions, currUpvotes, currRank24h, currMentions24h);
+			
+			// Map the current ticker to the current Crypto object
+			tickerToCryptoInfo.put(currTicker, currCrypto);
+			
+			// Assign the ith top 100 ticker
+			top100Tickers[i] = currTicker;
+		}
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println(this.tickerToCryptoInfo.get(this.top100Tickers[i]) + "\n");
+		}
+		
+		// Close the BufferedReader
+		file.close();
+	}
+}
